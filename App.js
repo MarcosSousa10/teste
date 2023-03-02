598363294c087d82a64d67165f6afcc9f96e3e10
Sr-Marcos <marcospegodesousa10@gmail.com>
2023-03-02 18:06:46 -0300

    asd

--- App.js
+++ App.js
@@ -7,14 +7,30 @@ import {
   StatusBar,
   Text,
   TouchableOpacity,
+  TextInput
 } from 'react-native';
 import Button from './src/components/Button';
 export default class App extends Component {
   state={
-    contador:80
+    contador:80,
+    name: "marcos",
+    personagens:[]
   }
   apertou= ()=>{
     this.setState({contador: this.state.contador+1})
+  }
+  mudouNome= nome=>{
+    this.setState({name:nome})
+  }
+  async componentWillUnmount(){
+    try{
+      const response = await fetch("https://swapi.co/api/people/?format=json");
+      const dataJson = await response.json();
+
+      this.setState({personagens: dataJson.results});
+    }catch(error){
+      Alert(error);
+    }
   }
 
   render() {
@@ -22,10 +38,19 @@ export default class App extends Component {
       <SafeAreaView>
         <StatusBar barStyle={'dark-content'} />
         <ScrollView>
+          <Text>{this.state.name}</Text>
           <Text>{this.state.contador}</Text>
+          <TextInput
+                placeholder  ='Nome:'
+                editable={true} 
+                value={this.state.name} onChangeText ={ nome=>this.mudouNome(nome)}
+                />
           <TouchableOpacity onPress={this.apertou}>
         <Text>Somar</Text>
       </TouchableOpacity>
+      {this.state.personagens.map(personagen =>{
+        return <Text key={personagen.name}>{personagen.name}</Text>;
+      })}
           <Button title="marcos" >
             <Text>Button</Text>
           </Button>
